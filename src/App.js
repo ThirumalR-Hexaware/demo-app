d7c056cfd3c5ce6dc5bad44192a323e183c91677
ThirumalR-Hexaware <42308@hexaware.com>
2019-10-29 14:58:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,103 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Slide from '@material-ui/core/Slide';
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Chart from "react-google-charts";
 
+
+function daysToMilliseconds(days) {
+  return days * 24 * 60 * 60 * 1000;
+}
+const columns = [
+  { type: "string", label: "Task ID" },
+  { type: "string", label: "Task Name" },
+  { type: "date", label: "Start Date" },
+  { type: "date", label: "End Date" },
+  { type: "number", label: "Duration" },
+  { type: "number", label: "Percent Complete" },
+  { type: "string", label: "Dependencies" }
+];
+
+const rows = [
+  [
+    "Research",
+    "Find sources",
+    new Date(2015, 0, 1),
+    new Date(2015, 0, 5),
+    null,
+    100,
+    null
+  ],
+  [
+    "Write",
+    "Write paper",
+    null,
+    new Date(2015, 0, 9),
+    daysToMilliseconds(3),
+    25,
+    "Research,Outline"
+  ],
+  [
+    "Cite",
+    "Create bibliography",
+    null,
+    new Date(2015, 0, 7),
+    daysToMilliseconds(1),
+    20,
+    "Research"
+  ],
+  [
+    "Complete",
+    "Hand in paper",
+    null,
+    new Date(2015, 0, 10),
+    daysToMilliseconds(1),
+    0,
+    "Cite,Write"
+  ],
+  [
+    "Outline",
+    "Outline paper",
+    null,
+    new Date(2015, 0, 6),
+    daysToMilliseconds(1),
+    100,
+    "Research"
+  ]
+];
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    position: 'relative',
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+}));
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 function App() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={{minHeight: 60}}> 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,6 +111,27 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Hello World
+      </Button>
+      <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Gantt Chart
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Chart
+          chartType="Gantt"
+          data={[columns, ...rows]}
+          width="100%"
+          height="50%"
+          legendToggle
+        />
+      </Dialog>
     </div>
   );
 }
